7a6bc72e56277c7edb62eb23ca062e6575ce8e58
Anshika15 <bansalanshika20@gmail.com>
2021-02-20 10:15:04 +0530

    Constarint the Request Mapping

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -14,16 +15,11 @@ import com.luv2code.springdemo.entity.Customer;
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// need to inject the customer dao
-	
-//	public CustomerController() {
-//		
-//	}
 
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel)
 	{
 		// get customers from the dao
